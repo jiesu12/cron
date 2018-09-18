c318ed8113c6fafe93d10633ed1458f70590b8e6
Jie Su <jiesu12@gmail.com>
2018-09-18 18:55:19 +0000

    use flock to prevent overlap

--- cron.sh
+++ cron.sh
@@ -4,9 +4,9 @@ crontab -r
 
 if [ "$FRESH_ENV" = 'true' ]
 then
-  echo -e "${SCHEDULE} su - jie -s /bin/sh -c '/job.sh >> /log/cron.log 2>&1'\n" | crontab -
+  echo -e "${SCHEDULE} flock -n /cron.lock su - jie -s /bin/sh -c '/job.sh >> /log/cron.log 2>&1'\n" | crontab -
 else
-  echo -e "${SCHEDULE} su jie -s /bin/sh -c '/job.sh >> /log/cron.log 2>&1'\n" | crontab -
+  echo -e "${SCHEDULE} flock -n /cron.lock su jie -s /bin/sh -c '/job.sh >> /log/cron.log 2>&1'\n" | crontab -
 fi
 
 crond -f
